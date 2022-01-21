5516684bf9d3293580f0eb43da5cecf7278b4f94
Eric Lima dos Santos <merick_lima@hotmail.com>
2022-01-21 13:15:57 -0300

    adicionado a função de transferencia entre contas de tipos diferentes e adicionado a função de transferencia via chave pix

--- src/app/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia.component.ts
@@ -21,8 +21,20 @@ export class TransferenciaComponent implements OnInit {
     this.dadosCliente();
   }
 
-  public transferir(_conta:String,_valor:String,_senha:String):void{
-    this.http.put("http://localhost:8080/conta/transferir?numCon="+this.cliente.conta.numeroConta+"&senha="+_senha+"&numConT="+_conta+"&vTrans="+_valor+"&idP="+this.cliente.id , this.cliente).subscribe();
+  public transferir(_conta:String,_valor:String,_senha:String,conta:String,contaAReceber:String,tipoT:String,chave:String):void{
+
+    if(conta == "Corrente" && contaAReceber == "Corrente" && tipoT == "Normal"){
+      this.http.put("http://localhost:8080/conta/transferir?numCon="+this.cliente.conta.numeroConta+"&senha="+_senha+"&numConT="+_conta+"&vTrans="+_valor+"&idP="+this.cliente.id , this.cliente).subscribe();
+    }else if(conta == "Corrente" && contaAReceber == "Poupanca" && tipoT == "Normal"){
+      this.http.put("http://localhost:8080/conta/transferirpoupanca?numCon="+this.cliente.conta.numeroConta+"&senha="+_senha+"&numConT="+_conta+"&vTrans="+_valor+"&idP="+this.cliente.id , this.cliente).subscribe();
+    }else if(conta == "Poupanca" && contaAReceber == "Poupanca" && tipoT == "Normal"){
+      this.http.put("http://localhost:8080/contapoupanca/transferir?numCon="+this.cliente.contaP.numeroConta+"&senha="+_senha+"&numConT="+_conta+"&vTrans="+_valor+"&idP="+this.cliente.id , this.cliente).subscribe();
+    }else if(conta == "Poupanca" && contaAReceber == "Corrente" && tipoT == "Normal"){
+      this.http.put("http://localhost:8080/contapoupanca/transferircorrente?numCon="+this.cliente.contaP.numeroConta+"&senha="+_senha+"&numConT="+_conta+"&vTrans="+_valor+"&idP="+this.cliente.id , this.cliente).subscribe();
+    }else{
+      this.http.put("http://localhost:8080/conta/transferirpix?chave="+chave+"&senha="+_senha+"&numConT="+this.cliente.contaP.numeroConta+"&vTrans="+_valor+"&idP="+this.cliente.id, this.cliente).subscribe();
+
+    }
     this.router.navigateByUrl('/usuario');
   }
 
